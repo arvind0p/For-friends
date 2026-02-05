1aec0106f002fc1bfe8be68bdb9cf59699e86cd6
arvind0p <133508538+arvind0p@users.noreply.github.com>
2026-02-05 11:42:24 +0530

    Add files via upload

---
+++ valentine-surprise/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return render_template("welcome.html")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/surprise")
+def surprise():
+    return render_template("surprise.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
